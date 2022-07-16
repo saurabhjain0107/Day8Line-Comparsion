5ffb064780b835b868a80e73ee91951cb8e14bf8
saurabh jain <luckyjain0107@gmai.com>
2022-07-16 14:39:03 +0530

    Calculate the length

--- LineComparsion1.java
+++ LineComparsion1.java
@@ -1,8 +1,6 @@
 package com.bridgelabz;
-
 import java.util.Scanner;
-
-public class LineComparsion {
+public class LineComparsion1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         float x1,y1,x2,y2;
@@ -14,5 +12,6 @@ public class LineComparsion {
         y2 = sc.nextFloat();
         double length = Math.sqrt( Math.pow((x2-x1), 2) + Math.pow((y2-y1), 2) );
         System.out.println(length);
+
     }
 }
