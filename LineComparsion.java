458a7c75a167c1d3cc7f735368598040d373b57c
saurabh jain <luckyjain0107@gmai.com>
2022-07-16 14:56:32 +0530

    compare two lines based on the end point

--- LineComparsion.java
+++ LineComparsion.java
@@ -2,6 +2,8 @@ package com.bridgelabz;
 
 import java.util.Scanner;
 
+import static java.lang.Double.compare;
+
 public class LineComparsion {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -12,20 +14,23 @@ public class LineComparsion {
         y1 = scanner.nextFloat();
         x2 = scanner.nextFloat();
         y2 = scanner.nextFloat();
-
-        double length1 = Math.sqrt( Math.pow((x2-x1), 2) + Math.pow((y2-y1), 2) );
+        double l1 = Math.sqrt( Math.pow((x2-x1), 2) + Math.pow((y2-y1), 2) );
 
         System.out.println("Enter x1 y1 x2 y2 of line two: ");
         x1 = scanner.nextFloat();
         y1 = scanner.nextFloat();
         x2 = scanner.nextFloat();
         y2 = scanner.nextFloat();
+        double l2 = Math.sqrt( Math.pow((x2-x1), 2) + Math.pow((y2-y1), 2) );
 
-        double length2 = Math.sqrt( Math.pow((x2-x1), 2) + Math.pow((y2-y1), 2) );
+        int x = compare(l1, l2);
+        System.out.println(x);
 
-        if (length1==length2){
+        if (x==0){
             System.out.println("Both the lines are equal in length. ");
-        }else
-            System.out.println("Both the lines are not equal in length. ");
+        }else if (x<0)
+            System.out.println("line one is smaller than line two. ");
+        else
+            System.out.println("line one is larger than line two. ");
     }
 }
